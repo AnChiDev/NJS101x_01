48c5f6b4a92fd8c0cefabf644679ba6767faac6a
MinhTran <designbyanchi@gmail.com>
2023-01-16 09:52:01 +0700

    Lab 2.4 Route

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const http = require('http');
+// const http = require('http');
 const express = require('express');
 const app = express();
 
-app.use((req, res, next)=>{
-    console.log('in the middleware');
-    next();
-});
 
-app.use((req, res, next)=>{
-    console.log('in another the middleware');
-    res.send('<h1>Hello from Express.js</h1>');
+
+app.use('/add-product',(req, res, next)=>{
+    res.send('<h1>The Add product page</h1>');
 });
-const server = http.createServer(app);
-server.listen(8000);
+app.use('/',(req, res, next)=>{
+    console.log('in the middleware');
+    res.send('<h1>Hello from Express.js</h1>')
+    
+});
+// const server = http.createServer(app);
+app.listen(8000);
