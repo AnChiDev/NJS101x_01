6f97ef387dc469e8dea878cb26340b4ac7d1aa73
MinhTran <designbyanchi@gmail.com>
2023-01-17 09:05:11 +0700

    using express router

--- index.js
+++ index.js
@@ -3,23 +3,26 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+const adminRoutes= require('./routes/admin');
+const shopRoutes= require('./routes/shop');
+
+
 app.use(bodyParser.urlencoded({extended: false}));
+
+
 
 // app.use('/',(req, res, next)=>{
 //     console.log('This always runs!');
 //     next();    
 // });
 
-app.use('/add-product',(req, res, next)=>{
-    res.send('<form action ="/product"><input type ="text" name ="title"><button type="submit">Submit</button></form>');
-});
-app.post('/product',(req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-})
-app.use('/',(req, res, next)=>{
-    console.log('in the middleware');
-    res.send('<h1>Hello from Express.js</h1>');
-});
+
+// app.use('/',(req, res, next)=>{
+//     console.log('in the middleware');
+//     res.send('<h1>Hello from Express.js</h1>');
+// });
 // const server = http.createServer(app);
+
+app.use(adminRoutes);
+app.use(shopRoutes);
 app.listen(8000);
---
+++ routes/admin.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/add-product', (req, res, next) => {
+res.send('<form action ="/product" method ="POST"><input type ="text" name ="title"><button type="submit">Submit</button></form>')
+})
+
+router.post('/product',(req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+
+module.exports = router;
