6e44ec6625976fa029bb712caf073ae44527fb8e
MinhTran <designbyanchi@gmail.com>
2023-01-17 10:14:23 +0700

    serving HTML pages and returning a 404 page

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,9 @@
+const path = require('path');
 const express = require('express');
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-res.send('<form action ="/admin/add-product" method ="POST"><input type ="text" name ="title"><button type="submit">Submit</button></form>')
-})
+res.sendFile(path.join(__dirname, '../','views','add-product.html'))})
 
 router.post('/product',(req, res, next) => {
     console.log(req.body);
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,10 @@
+const path = require('path');
 const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next)=>{
-    res.send(
-        '<h1> Hello from express.js</h1>'
+    res.sendFile(
+        path.join(__dirname,'../','views','shop.html')
     )
 })
 
