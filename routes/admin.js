c16d34fe9370e40b4e97b34a577d65a170d6970f
MinhTran <designbyanchi@gmail.com>
2023-02-01 09:22:15 +0700

    sending data across requests and user

--- routes/admin.js
+++ routes/admin.js
@@ -3,13 +3,18 @@ const express = require('express');
 const rootDir = require('../util/path');
 const router = express.Router();
 
+const products =[];
+
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-  });
-  
-  // /admin/add-product => POST
-  router.post('/add-product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-  });
-module.exports = router;
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+});
+
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect('/');
+});
+
+exports.routes = router;
+exports.products = products;
