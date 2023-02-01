981b5d493be36174144662423d934f4ea5bcab6d
MinhTran <designbyanchi@gmail.com>
2023-02-01 15:13:40 +0700

    Finishing the Pug Template

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404');
+    res.status(404).render('404',{pageTitle: 'Page not Found'});
 });
 
 
