0b8ea740b4877caaefad38b0a989b85c51cd59a6
MinhTran <designbyanchi@gmail.com>
2023-02-01 13:35:26 +0700

    Outputting Dynamic Content

--- routes/shop.js
+++ routes/shop.js
@@ -7,7 +7,8 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-res.render('shop');
+  const products = adminData.products;
+res.render('shop', {prods: products, docTitle: 'Shop'});
   });
   
 
