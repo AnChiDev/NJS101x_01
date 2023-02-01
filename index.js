5f8d018459ebcd906a191d7b38fcfbca3f5cd081
MinhTran <designbyanchi@gmail.com>
2023-02-01 10:29:48 +0700

    installing and implementing pug

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views','views');
 
 const shopRoutes = require('./routes/shop');
 const adminData = require('./routes/admin');
--- routes/shop.js
+++ routes/shop.js
@@ -7,8 +7,7 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    console.log('shop.js',adminData.products);
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+res.render('shop');
   });
   
 
