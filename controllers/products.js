b5ca56c37f8ffdd5ed3cda1c1abf366edbdddad5
MinhTran <designbyanchi@gmail.com>
2023-02-09 13:32:58 +0700

    storing data in files via the model and fetching data files via the model

--- controllers/products.js
+++ controllers/products.js
@@ -18,13 +18,15 @@ exports.getAddProduct=(req, res, next) => {
 
 
  exports.getProducts= (req, res, next) => {
-    const products = Product.fetchAll()
-    res.render('shop', {
-      prods: products,
-      pageTitle: 'Shop',
-      path: '/',
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true
-    });
+     Product.fetchAll(products=>{
+        res.render('shop', {
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/',
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true
+      });
+    })
+   
   };
--- models/product.js
+++ models/product.js
@@ -1,13 +1,36 @@
-const products=[];
+const  fs = require('fs');
+const path = require('path');
+
+
 module.exports = class Product{
     constructor(t){
         this.title= t;
     }
     save(){
-        products.push(this);
+       
+        fs.readFile(p,(err, fileContent)=>{
+            let products =[];
+           if(!err){
+                products = JSON.parse(fileContent);
+           }
+           products.push(this);
+           fs.writeFile(p, JSON.stringify(products), (err)=>{
+            console.log(err);
+           });
+        });
     }
 
-    static fetchAll(){
-        return products;
+    static fetchAll(cb){
+        const p = path.join(
+            path.dirname(process.mainModule.filename),
+            'data',
+            'products.json'
+            );
+       fs.readFile(p,(err,fileContent)=>{
+        if (err){
+          cb([]);
+        }
+        cb(JSON.parse(fileContent));
+       })
     }
 }
