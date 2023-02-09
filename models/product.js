a7b288546ff6167df125a5e400f4ed7207323957
MinhTran <designbyanchi@gmail.com>
2023-02-09 10:09:14 +0700

    adding a product model

---
+++ models/product.js
@@ -0,0 +1,13 @@
+const products=[];
+module.exports = class Product{
+    constructor(t){
+        this.title= t;
+    }
+    save(){
+        products.push(this);
+    }
+
+    static fetchAll(){
+        return products;
+    }
+}
