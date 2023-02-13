3b937834288fc1cb6c9276d576161777e75d243d
MinhTran <designbyanchi@gmail.com>
2023-02-13 15:28:42 +0700

    Editing the product data

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,12 +1,10 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
+  res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
+    editing: false,
   });
 };
 
@@ -15,10 +13,40 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null,title, imageUrl, description, price);
   product.save();
   res.redirect('/');
 };
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect('/');
+  }
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    if (!product) {
+      return res.redirect('/');
+    }
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product: product
+    });
+  });
+};
+
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  const updateTitle = req.body.title;
+  const updatePrice=  req.body.price;
+  const updateImage = req.body.image;
+  const updateDesc= req.body.description;
+  const updatedProduct = new Product(prodId, updateTitle, updatePrice, updateImage, updateDesc);
+  updatedProduct.save();
+  res.redirect('/admin/products');
+}
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -21,7 +22,6 @@ exports.getProduct = (req, res, next) => {
   });
 };
 
-
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('shop/index', {
@@ -39,6 +39,13 @@ exports.getCart = (req, res, next) => {
   });
 };
 
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect('/cart');
+};
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
     path: '/orders',
